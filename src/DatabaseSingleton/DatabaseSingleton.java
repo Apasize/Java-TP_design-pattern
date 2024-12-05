9b938cb74074aedf825a46ea2361fb2f532b0d71
Apasize <agbozognigbeandy3@gmail.com>
2024-12-05 11:22:27 +0000

    exercice 2 done

---
+++ src/DatabaseSingleton/DatabaseSingleton.java
@@ -0,0 +1,30 @@
+package DatabaseSingleton;
+
+public class DatabaseSingleton {
+    private static DatabaseSingleton instance;
+
+    private int record;
+    private String name;
+
+
+    private DatabaseSingleton(String name) {
+        this.record = 0;
+        this.name = name;
+    }
+
+    public static DatabaseSingleton getInstance(String name) {
+        if (instance == null) {
+            instance = new DatabaseSingleton(name);
+        }
+        return instance;
+    }
+
+    public void editRecord(int recordNumber) {
+        this.record = recordNumber;
+        System.out.println("\nEnregistrement numéro " + recordNumber + " modifié dans la base de données " + name);
+    }
+
+    public String getName(){
+        return this.name;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,22 @@
+import DatabaseSingleton.DatabaseSingleton;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("...Test de l'unicité de l'instance DatabaseSingleton...");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        DatabaseSingleton databaseSingleton = DatabaseSingleton.getInstance("ApasizeDatabase");
+
+        databaseSingleton.editRecord(6);
+
+        System.out.println("\nNom de la base de données : " + databaseSingleton.getName());
+
+
+        DatabaseSingleton newDatabaseSingleton = DatabaseSingleton.getInstance("AMZDatabase");
+
+        System.out.println("\nNom de la nouvelle base de données : "+ newDatabaseSingleton.getName());
+
+        System.out.println("\nLes deux base de données sont elles indentiques ? "+ (databaseSingleton == newDatabaseSingleton));
     }
 }
